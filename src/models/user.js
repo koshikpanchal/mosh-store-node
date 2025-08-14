0623b6c13d1ea659ffa229e66c505d6b5a29015a
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-14 10:29:54 +0530

    added access and refresh token

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, index: true },
@@ -27,6 +29,52 @@ const userSchema = new mongoose.Schema({
     country: { type: String },
   },
 });
+
+userSchema.methods.isValidPassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+  return isPasswordValid;
+};
+
+userSchema.methods.getRefreshToken = async function () {
+  const user = this;
+  const token = await jwt.sign(
+    {
+      _id: user._id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.email,
+      role: user.role,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  return token;
+};
+
+userSchema.methods.getAccessToken = async function () {
+  const user = this;
+  const token = await jwt.sign(
+    {
+      _id: user._id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.email,
+      role: user.role,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "15m",
+    }
+  );
+
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const { validateSignupData } = require("../validator/validator");
-const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 const authRoutes = express.Router();
@@ -11,8 +9,20 @@ authRoutes.post("/login", async (req, res) => {
 
     // fetching user from database
     const user = await User.findOne({ email: email });
+    if (await user.isValidPassword(password)) {
+      const accessToken = await user.getAccessToken();
+      const refreshToken = await user.getRefreshToken();
 
-    res.json({ data: user });
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        path: "/auth/refresh",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        secure: true,
+      });
+      res.json({ token: accessToken });
+    } else {
+      throw new Error("Invalid credentials");
+    }
   } catch (error) {
     console.error("error in registering user", error);
   }
