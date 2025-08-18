d67bd966c754d4be5539357ee234aa2806ceee59
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-18 12:11:21 +0530

    added get mapping for finding products of a paricular category and also with product ID

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,7 +9,7 @@ const Product = require("../models/product");
 const productRoutes = express.Router();
 
 productRoutes.post(
-  "/",
+  "/add",
   authMiddleware,
   adminAuthMiddleware,
   async (req, res) => {
@@ -44,5 +44,49 @@ productRoutes.post(
     }
   }
 );
+
+// Get all products by categoryId
+productRoutes.get("/by-category/:categoryId", async (req, res) => {
+  try {
+    const { categoryId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+      return res.status(400).json({ error: "Invalid categoryId" });
+    }
+
+    const products = await Product.find({ categoryId }).populate("categoryId", [
+      "name",
+    ]);
+
+    res.json({ data: products });
+  } catch (error) {
+    console.error("Error fetching products", error);
+    res.status(500).json({ error: "Error fetching products" });
+  }
+});
+
+//get product by productId
+productRoutes.get("/by-id/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ error: "Invalid productId" });
+    }
+
+    const product = await Product.findById(productId).populate("categoryId", [
+      "name",
+    ]);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json({ data: product });
+  } catch (error) {
+    console.error("Error fetching product", error);
+    res.status(500).json({ error: "Error fetching product" });
+  }
+});
 
 module.exports = productRoutes;
